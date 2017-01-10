8a55f2381c56f492e27822812bd73095d0a26189
Jérémie Litzler <contact@jeremielitzler.net>
2017-01-10 07:21:29 +0100

    static site

--- modules/constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-    define("LANG_FR", "fr");
-    define("PAGE_TITLE", "page_title");
--- modules/translations.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once("constants.php");
-    $translations = [
-        LANG_FR => [
-            PAGE_TITLE => "Vignal en chanson",
-            ],
-    ];
