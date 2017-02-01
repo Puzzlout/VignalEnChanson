dc09dd50c89a111af23333c608cbb68f88792670
Jérémie Litzler <contact@jeremielitzler.net>
2017-02-01 06:19:07 +0100

    gitignore update

--- php/layout.php
+++ php/layout.php
@@ -2,21 +2,21 @@
 <html lang="fr">
 
 <?php
-    include_once("modules/1.head.php");
+require("modules/1.head.php");
 ?>
 
 <body id="page-top" class="index">
 
 <?php
-    include_once("modules/2.nav.php");
-    include_once("modules/3.header.php");
-    include_once("modules/4.team.php");
-    include_once("modules/5.music.php");
-    include_once("modules/X.gallery.php");
-    include_once("modules/7.contact.php");
-    include_once("modules/8.footer.php");
-    include_once("modules/9.modals.php");
-    include_once("modules/10.js.php");
+require("modules/2.nav.php");
+require("modules/3.header.php");
+require("modules/4.team.php");
+require("modules/5.music.php");
+require("modules/6.school.php");
+require("modules/7.contact.php");
+require("modules/8.footer.php");
+require("modules/9.modals.php");
+require("modules/10.js.php");
 ?>
 
 </body>
--- php/modules/5.music.php
+++ php/modules/5.music.php
@@ -1,86 +1,88 @@
 <!-- Music Section -->
 <section id="music">
-    <div class="container">
-        <div class="row music-header">
-            <div class="col-lg-12 text-center">
-                <h1 class="section-heading">Extraits</h1>
-                <h2 class="section-subheading text-muted">Vidéo, enregistrement audio, photos.</h2>
+    <div class="hero">
+        <div class="container">
+            <div class="row music-header">
+                <div class="col-lg-12 text-center">
+                    <h1 class="section-heading">Extraits</h1>
+                    <h2 class="section-subheading text-muted">Vidéo, enregistrement audio, photos.</h2>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="container-fluid">
-                    <div class="row video-content">
-                        <div class="col-lg-12">
-                            <a href="https://www.youtube.com/" target="_blank">
-                                <i class="fa fa-youtube-square"></i>
-                                <p>Rejoignez notre chaine YouTube</p>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row music-content">
-                        <div class="player col-lg-12">
-                            <div class="audio-wrapper">
-                                <audio id="audio" tabindex="0" preload="auto" controls="controls">
-                                    <source type="audio/mp3"
-                                            src="https://1268182399.rsc.cdn77.org/media/audio/1-approche.mp3"/>
-                                    <div class="audio-no-html5">
-                                        <p>Désolé, votre navigateur ne permet pas de jouer notre musique. Veuillez
-                                            télécharger l'un
-                                            des navigateurs ci-dessous :</p>
-                                        <ul>
-                                            <li><a href="https://www.google.fr/chrome">Google Chrome</a></li>
-                                            <li><a href="https://www.mozilla.org/fr/firefox/new/">Firefox</a></li>
-                                            <li><a href="http://www.opera.com/fr">Opera</a></li>
-                                        </ul>
-                                    </div>
-                                </audio>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="container-fluid">
+                        <div class="row video-content">
+                            <div class="col-lg-12">
+                                <a href="https://www.youtube.com/" target="_blank">
+                                    <i class="fa fa-youtube-square"></i>
+                                    <p>Rejoignez notre chaine YouTube</p>
+                                </a>
                             </div>
-                            <div class="audio-list-wrapper">
-                                <div class="playlist-controls"></div>
-                                <ul id="playlist">
-                                    <li class="audio-track audio-track-even active">
-                                        <a href="https://1268182399.rsc.cdn77.org/media/audio/1-approche.mp3">
+                        </div>
+                        <div class="row music-content">
+                            <div class="player col-lg-12">
+                                <div class="audio-wrapper">
+                                    <audio id="audio" tabindex="0" preload="auto" controls="controls">
+                                        <source type="audio/mp3"
+                                                src="https://1268182399.rsc.cdn77.org/media/audio/1-approche.mp3"/>
+                                        <div class="audio-no-html5">
+                                            <p>Désolé, votre navigateur ne permet pas de jouer notre musique. Veuillez
+                                                télécharger l'un
+                                                des navigateurs ci-dessous :</p>
+                                            <ul>
+                                                <li><a href="https://www.google.fr/chrome">Google Chrome</a></li>
+                                                <li><a href="https://www.mozilla.org/fr/firefox/new/">Firefox</a></li>
+                                                <li><a href="http://www.opera.com/fr">Opera</a></li>
+                                            </ul>
+                                        </div>
+                                    </audio>
+                                </div>
+                                <div class="audio-list-wrapper">
+                                    <div class="playlist-controls"></div>
+                                    <ul id="playlist">
+                                        <li class="audio-track audio-track-even active">
+                                            <a href="https://1268182399.rsc.cdn77.org/media/audio/1-approche.mp3">
                                 <span>
                                     <i class="fa fa-play" aria-hidden="true"></i>
                                 </span>
-                                            <span>
+                                                <span>
                                     <span class="audio-track-name">Approche, Approche</span>
                                 </span>
-                                        </a>
-                                    </li>
-                                    <li class="audio-track audio-track-odd ">
-                                        <a href="https://1268182399.rsc.cdn77.org/media/audio/2-un-beau-tresor.mp3">
-                                            <i class="fa fa-play" aria-hidden="true"></i>
-                                            <span>Un beau trésor</span>
-                                        </a>
-                                    </li>
-                                </ul>
+                                            </a>
+                                        </li>
+                                        <li class="audio-track audio-track-odd ">
+                                            <a href="https://1268182399.rsc.cdn77.org/media/audio/2-un-beau-tresor.mp3">
+                                                <i class="fa fa-play" aria-hidden="true"></i>
+                                                <span>Un beau trésor</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 hide-from-768w">
-                <div class="col-lg-6 gallery-item">
-                    <a href="#galleryModal1" class="gallery-link" data-toggle="modal">
-                        <div class="gallery-hover">
-                            <div class="gallery-hover-content">
-                                <i class="fa fa-expand fa-3x"></i>
+                <div class="col-md-6 hide-from-768w">
+                    <div class="col-lg-6 gallery-item">
+                        <a href="#galleryModal1" class="gallery-link" data-toggle="modal">
+                            <div class="gallery-hover">
+                                <div class="gallery-hover-content">
+                                    <i class="fa fa-expand fa-3x"></i>
+                                </div>
                             </div>
-                        </div>
-                        <img src="img/gallery/1-preview.jpg" class="img-responsive" alt="Photo Galerie 1">
-                    </a>
-                </div>
-                <div class="col-lg-6 gallery-item">
-                    <a href="#galleryModal4" class="gallery-link" data-toggle="modal">
-                        <div class="gallery-hover">
-                            <div class="gallery-hover-content">
-                                <i class="fa fa-expand fa-3x"></i>
+                            <img src="img/gallery/1-preview.jpg" class="img-responsive" alt="Photo Galerie 1">
+                        </a>
+                    </div>
+                    <div class="col-lg-6 gallery-item">
+                        <a href="#galleryModal4" class="gallery-link" data-toggle="modal">
+                            <div class="gallery-hover">
+                                <div class="gallery-hover-content">
+                                    <i class="fa fa-expand fa-3x"></i>
+                                </div>
                             </div>
-                        </div>
-                        <img src="img/gallery/4-preview.jpg" class="img-responsive" alt="Photo Galerie 4">
-                    </a>
+                            <img src="img/gallery/4-preview.jpg" class="img-responsive" alt="Photo Galerie 4">
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
